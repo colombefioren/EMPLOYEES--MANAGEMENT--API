13b8ec18f12538ac27486605ba9c831d37527e60
colombefioren <colomberakotonjanahary@gmail.com>
2026-05-31 12:33:50 +0300

    fix: remove intern logic and re add employee migration to be in order

--- src/main/java/org/coco/jpa/repository/model/Employee.java
+++ src/main/java/org/coco/jpa/repository/model/Employee.java
@@ -1,6 +1,8 @@
 package org.coco.jpa.repository.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import java.math.BigDecimal;
 import lombok.Getter;
@@ -21,11 +23,13 @@ public class Employee {
   private String firstName;
 
   @Column(unique = true)
+  @Email(message = "Invalid email format")
   private String email;
 
   @Enumerated(EnumType.STRING)
   private Department department;
 
+  @DecimalMin(value = "1500.0", message = "Salary must be at least 1500?")
   private BigDecimal salary;
 
   @Column(name = "is_active")
